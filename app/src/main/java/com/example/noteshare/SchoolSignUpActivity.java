a13f67342fc97821a127cf46921979ee32ec6f0c
Nima Yonten <nimayonten@gmail.com>
2020-07-14 16:18:18 +0530

    SchoolSignUpActivity

---
+++ app/src/main/java/com/example/noteshare/SchoolSignUpActivity.java
@@ -0,0 +1,17 @@
+package com.example.noteshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class SchoolSignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_school_sign_up);
+
+
+    }
+}
